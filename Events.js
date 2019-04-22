8f7fc885e3512e58cb20eaa8a254ade496ab88f0
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-04-22 10:48:18 +0530

    Evenets in JavaScript

---
+++ Events.js
@@ -0,0 +1,31 @@
+
+/***
+ Example - 01 : Using onclick() event
+
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1 onclick="say(this)">Click here</h1>
+
+</body>
+<script>
+    function say() { 
+        alert("Hello");
+    }
+</script>
+</html>
+
+***/
+
+
+
+
+/***
+ NOTE: for more inforamtion please visit
+ https://www.w3schools.com/jsref/dom_obj_event.asp
+ 
+ ***/
+
+
+ 
