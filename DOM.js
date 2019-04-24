17bbffb8d5aee400dc26d48b2c927842d9439afd
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-04-24 10:28:51 +0530

    DOM Manipulation in Vanilla JavaScript

--- DOM.js
+++
@@ -1,9 +0,0 @@
-
-
-document.getElementById("divId").innerHTML = "Hello World!"; //Assigning new value
-
-var str = document.getElementsByTagName("div").value; //Get div value
-
-var str = document.getElementsByClassName("first").value; //Get div value using class "first"
-
-document.getElementById("divId").style.color = "red"; //Applying css style to div
